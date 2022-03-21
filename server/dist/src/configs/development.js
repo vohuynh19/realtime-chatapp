ca066a06dc17195a91851b9650de1a1ab27e66bd
vohuynh19 <vohuynh01092002@gmail.com>
2022-03-21 19:39:24 +0700

    edit server gitignore

--- server/dist/src/configs/development.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const base_1 = __importDefault(require("./base"));
-exports.default = Object.assign(Object.assign({}, base_1.default), { env: "development", maindb: process.env["MONGODB_URI"], debug: true });
-//# sourceMappingURL=development.js.map
